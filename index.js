2bb958a6813103cb40e7067c90609bfd8e41d041
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-25 01:45:38 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-require.apply('dotenv').config();
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // midell ware 
-
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.6euue.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('bangali-industry').collection('users');
+
+
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Hey Man")
