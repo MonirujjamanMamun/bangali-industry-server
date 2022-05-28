9d08d03ec01d2a035785a1302f7eea215008dcc8
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-28 23:46:36 +0600

    add product added

--- index.js
+++ index.js
@@ -165,6 +165,11 @@ async function run() {
             const result = await ordersCollection.deleteOne(query)
             res.send(result)
         })
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product)
+            res.send(result)
+        })
 
 
 
